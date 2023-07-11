df306c2a8eae0ea53d288817ec5fedd9306f7fa8
LC_Hsuan <jackie6388@hotmail.com.tw>
2023-07-11 14:26:27 +0800

    rename the file

--- distortion.py
+++ distortion.py
@@ -74,7 +74,7 @@ def main(args):
     resized_img = cv2.resize(cv_img2, (cv_img.shape[1], cv_img.shape[0]), interpolation=cv2.INTER_LINEAR)
     cv2.imwrite("./result_original.png", cv_img2)
     cv2.imwrite("./result_unscale.png", resized_img)
-    print("Image stored at ./result_scaled.png and ./result_rescaled.png")
+    print("Image stored at ./result.png and ./result_rescaled.png")
 
 if __name__ == "__main__":
     args = parse_args()
